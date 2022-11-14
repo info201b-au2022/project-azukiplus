5d424dbaf37b4ad7da8f315c20cd324c241f985c
GGB0N0D <zli2003@uw.edu>
2022-11-13 16:51:27 -0800

    Added .sav source data file to data folder

--- data/datastorage.R
+++ data/datastorage.R
@@ -1,6 +1,5 @@
 install.packages("foreign")
 library(foreign)
 
-data <- read.spss("C:/Users/**your-username**/Downloads/dataverse_files/2018_2019_ANP_online.sav", to.data.frame=TRUE)
+data <- read.spss("C:/Users/**username**/Downloads/dataverse_files/2018_2019_ANP_online.sav", to.data.frame=TRUE)
 View(data)
-
--- docs/summary-table.R
+++ docs/summary-table.R
@@ -1,5 +1,17 @@
-# 
-#
-#
-#
+# P2-Exploratory Analysis
+# INFO-201 B
+# Instructor: David G Hendry
+# TA: Raina Scherer
+# Group Member: Ryan William Breuer, Vincent Li, Ashley Mochizuku
+# Aggregate Table Script
 
+# load packages
+library("haven")
+library("tidyverse")
+
+# get data file
+data <- read_sav("C:/Users/vl206/Documents/info201/projects/project-01/project-azukiplus/data/2018_2019_ANP_online.sav")
+View(data)
+
+features <- ncol(data)
+participants <- nrow(data)
