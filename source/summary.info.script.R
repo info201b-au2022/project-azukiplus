c3572707806f8eb3b244e020e50693430417716a
azukiplus <ashleymochizuki@gmail.com>
2022-11-14 21:29:55 -0800

    Add summary info script and fix chart2 dependencies

--- source/summary.info.script.R
+++ source/summary.info.script.R
@@ -1,15 +1,38 @@
 # summary_info.R 
 # A source file that takes in a dataset and returns a list of info about it:
+library(RCurl)
+library(dplyr)
 
-
-library(foreign)
-mydata = read.spss("C:\\users\\ryanbreuer\\documents\\info201\\project-azukiplus\\", to.data.frame= true)
-
-install.packages(2018_2019_ANP_online.sav)
+raw = getURL("https://raw.githubusercontent.com/info201b-au2022/project-azukiplus/main/data/data.csv")
+data <- read.csv(text = raw)
 
 summary_info <- list()
-summary_info$num_observations <- nrow(my_dataframe)
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
 
+# gets the number of observations
+summary_info$num_observations <- nrow(data)
+
+# gets the number of observations that has used drugs while gambling
+summary_info$drug_usage <- data %>%
+  filter(!is.na(b_gc5)) %>%
+  filter(b_gc5 != 0) %>%
+  nrow()
+
+# gets the number of observations that has used alcohol while gambling
+summary_info$alcohol_usage <- data %>%
+  filter(!is.na(b_gc2)) %>%
+  filter(b_gc2 != 0) %>%
+  nrow()
+
+# gets the number of observations that have an immediate family member with a gambling problem
+# excludes "unsure" answers
+summary_info$immediate_family <- data %>%
+  filter(!is.na(b_gfh1a)) %>%
+  filter(b_gfh1a == 1) %>%
+  nrow()
+
+# gets the number of observations that have developed a serious physical illness, serious mental illness,
+# or drug or alcohol addiction 
+summary_info$health_problems <- data %>%
+  filter(!is.na(b_c10_48) | !is.na(b_c10_49) | !is.na(b_c10_50)) %>%
+  filter(b_c10_48 == 1| b_c10_49 == 1| b_c10_50 == 1) %>%
+  nrow()
